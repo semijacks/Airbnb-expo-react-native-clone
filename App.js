7970356f810fbbf1b5f963dc3d3ef7d40d4215d5
Semilore Idowu <semijacks27@gmail.com>
2021-04-02 12:52:08 +0100

    added custom fonts

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { StatusBar, SafeAreaView } from "react-native";
+import { StatusBar, SafeAreaView, Text } from "react-native";
 import HomeScreen from "./src/screens/Home";
 import Post from "./src/components/Post";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
